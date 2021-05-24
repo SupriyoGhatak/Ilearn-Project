9c0af2a7f636e43c653c148866b054e5b793a2c4
Sanchari99 <sanchari1999@gmail.com>
2021-05-24 18:48:09 +0530

    ok

--- payment.php
+++ payment.php
@@ -18,6 +18,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
     <!-- main css -->
     <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/stylee.css"/>
     
   </head>
 
@@ -27,7 +28,7 @@
         include 'header_index.php';
     ?>
     <!--================ End Header Menu Area =================-->
-    <div class="popular_courses section_gap_top">  
+    <!--<div class="popular_courses section_gap_top">  
     <form>
         <table class="table table-borderless">
             <thead>
@@ -60,8 +61,107 @@
            </table>
        
     </form>
-    </div>
+    </div> -->
     
+    
+    <div class="popular_courses section_gap_top">  
+    <form>
+        <!-- <table class="table table-borderless">
+            <thead>
+                    <tr>
+                      <th scope="col"></th>
+                      <th scope="col">First</th>
+                      <th scope="col"></th>
+                      
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row"></th>
+                      <td>Mark</td>
+                      
+                      <td>@mdo</td>
+                    </tr>
+                    <tr>
+                      <th scope="row"></th>
+                      <td>Jacob</td>
+                      
+                      <td>@fat</td>
+                    </tr>
+                    <tr>
+                      <th scope="row"></th>
+                      <td colspan="2">Larry the Bird</td>
+                      
+                    </tr>
+                  </tbody>
+           </table>
+       -->
+       <div class="row">
+  <div class="col-75">
+    <div class="container">
+      <form action="/action_page.php">
+
+        <div class="row">
+          <div class="col-50">
+            <h3>Billing Address</h3>
+            <label for="fname"><i class="fa fa-user"></i> Full Name</label>
+            <input type="text" id="fname" name="firstname" placeholder="EnterName">
+            <label for="email"><i class="fa fa-envelope"></i> Email</label>
+            <input type="text" id="email" name="email" placeholder="abc@xyz.com">
+            <label for="adr"><i class="fa fa-address-card-o"></i> Address</label>
+            <input type="text" id="adr" name="address" placeholder="EnterAddress">
+            <label for="city"><i class="fa fa-institution"></i> City</label>
+            <input type="text" id="city" name="city" placeholder="EnterCity">
+
+            <div class="row">
+              <div class="col-50">
+                <label for="state">State</label>
+                <input type="text" id="state" name="state" placeholder="EnterState">
+              </div>
+              <div class="col-50">
+                <label for="zip">Zip</label>
+                <input type="text" id="zip" name="zip" placeholder="100001">
+              </div>
+            </div>
+          </div>
+
+           <div class="col-50">
+            <h3>Payment</h3>
+            <label for="fname">Accepted Cards</label>
+            <div class="icon-container">
+              <i class="fa fa-cc-visa" style="color:navy;"></i>
+              <i class="fa fa-cc-amex" style="color:blue;"></i>
+              <i class="fa fa-cc-mastercard" style="color:red;"></i>
+              <i class="fa fa-cc-discover" style="color:orange;"></i>
+            </div>
+            <label for="cname">Name on Card</label>
+            <input type="text" id="cname" name="cardname" placeholder="Enter Name">
+            <label for="ccnum">Credit card number</label>
+            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444">
+            <label for="expmonth">Exp Month</label>
+            <input type="text" id="expmonth" name="expmonth" placeholder="September">
+
+            <div class="row">
+              <div class="col-50">
+                <label for="expyear">Exp Year</label>
+                <input type="text" id="expyear" name="expyear" placeholder="2018">
+              </div>
+              <div class="col-50">
+                <label for="cvv">CVV</label>
+                <input type="text" id="cvv" name="cvv" placeholder="352">
+              </div>
+            </div>
+          </div> 
+
+        </div>
+        <label>
+          <input type="checkbox" checked="checked" name="sameadr"> Shipping address same as billing
+        </label>
+          <input type="submit" value="Continue to checkout" class="btn">
+         <!-- <a href="cardinfo.php" class="btn">Continue to checkout</a> -->
+      </form>
+    </div>
+  </div>
     
     
      <?php 
