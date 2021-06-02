d674771ec64bdc9a9a98734483b055c83d4df8f4
Sanchari99 <sanchari1999@gmail.com>
2021-06-02 10:52:11 +0530

    k

--- payment_script.php
+++ payment_script.php
@@ -1,42 +1,5 @@
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
-    />
-    <link rel="icon" href="img/logo_1.png" style="height: 40px; width: 40px;" type="image/png" />
-    <title>Ilearn Education</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.css" />
-    <link rel="stylesheet" href="css/flaticon.css" />
-    <link rel="stylesheet" href="css/themify-icons.css" />
-    <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css" />
-    <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css" />
-    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
-    
-   
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- main css -->
-    <link rel="stylesheet" href="css/style.css" />
-    <link rel="stylesheet" href="css/stylee.css"/>
-    
-  </head>
 
-  <body>
-<?php
- include 'header_index.php';
-    ?>
-      
-  <div class="popular_courses section_gap_top">   
-    <div class="container"> 
-        <div class="row"> 
-          <div class="col-50">
-            <h3>Billing Address</h3>  
- 
-  </body>
-</html>
+
 <?php
 $con = mysqli_connect("localhost","root","","payment") or die(mysqli_error($con));
 
