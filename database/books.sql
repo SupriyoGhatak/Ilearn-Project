41fd3d6330452b38e8fef14d8c4a4bb9b0ef61f1
Sanchari99 <sanchari1999@gmail.com>
2021-06-23 13:12:07 +0530

    done

--- database/books.sql
+++ database/books.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 21, 2021 at 02:17 PM
+-- Generation Time: Jun 23, 2021 at 07:39 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -38,15 +38,17 @@ CREATE TABLE IF NOT EXISTS `books` (
   `image_path` text NOT NULL,
   `pdf_path` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `books`
 --
 
 INSERT INTO `books` (`id`, `category_ids`, `title`, `author`, `description`, `image_path`, `pdf_path`) VALUES
-(1, '1,1', 'Let Us C', 'Yashavant Kanetkar', 'The bible for C Programming Language for all beginners to learn from.', 'https://drive.google.com/file/d/1HEcgV_czYlxB6jVVL3HS6iPBh6dtstxL/preview', 'https://drive.google.com/file/d/1TfwdQTpe5yM-blROGGJ5nYibCW26qm4H/preview'),
-(2, '2,2', 'The Complete Reference- C++', 'Herbert Schildt', 'Covers all the topics starting from beginners to advanced.', 'https://drive.google.com/file/d/1sTA3rslrh4S0XCIuy-oVcEEBrjDxnxPJ/preview', 'https://drive.google.com/file/d/1E573_dCaYtfPs-J8342y7nAUpqi_ectU/preview');
+(1, '1,1', 'Let Us C', 'Yashavant Kanetkar', 'The bible for C Programming Language for all beginners to learn from.', 'https://drive.google.com/file/d/1X_GW9jEyZNRyuDiNXzpfAh4CPr0Kr53W/preview', 'https://drive.google.com/file/d/1TfwdQTpe5yM-blROGGJ5nYibCW26qm4H/preview'),
+(2, '2,2', 'The Complete Reference- C++', 'Herbert Schildt', 'Covers all the topics starting from beginners to advanced.', 'https://drive.google.com/file/d/1FRxDhL-LA5jpHVffJ4apahEDRahYKFiD/preview', 'https://drive.google.com/file/d/1E573_dCaYtfPs-J8342y7nAUpqi_ectU/preview'),
+(3, '3,3', 'The Complete Reference- JAVA', 'Herbert Schildt', 'Comprehensive guide to the entire Java language with hundreds of examples.', 'https://drive.google.com/file/d/1biFFKvHiG1B-twebujCyy8_DRGL5vXQe/preview', 'https://drive.google.com/file/d/1pDhzy1nyqCRny8ojzalGIhXvYqJwWDY6/preview'),
+(4, '4,4', 'Fundamentals of Relational Databases', 'Ramon A. Mata-Toledo and Pauline K. Cushman', 'Full coverage of the database systems with complete scripts.', 'https://drive.google.com/file/d/1TKqp0Z9k9w4FqmjwS2Tcaf6BRf1B3yQx/preview', 'https://drive.google.com/file/d/1cGy1ifQETcJgd7L3sryVRySYjDMkQmPh/preview');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
