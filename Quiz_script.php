c48fe37242c354db474837c22df453b3e4825b53
tanmoy19 <84314512+tanmoy19@users.noreply.github.com>
2021-06-23 10:12:55 +0530

    done quiz backend

--- Quiz_script.php
+++ Quiz_script.php
@@ -11,7 +11,7 @@ include 'Connection/common.php';
             if(!empty($_POST['quizcheck'])) {
             // Counting number of checked checkboxes.
             $checked_count = count($_POST['quizcheck']);
-             print_r($_POST['quizcheck']);
+            // print_r($_POST['quizcheck']);
             $selected = $_POST['quizcheck'];
             
             $q1= " select ans_id from question WHERE  type = '".$type."' AND course = '".$course."'";
@@ -21,7 +21,7 @@ include 'Connection/common.php';
             $i = 1;
             while($rows = mysqli_fetch_array($ansresults)) {
                
-               print_r($rows);
+              // print_r($rows);
                //echo $rows['ans_id'];
               // print_r ($selected[$i] );
             	$flag = $rows['ans_id'] == $selected[$i];
@@ -42,14 +42,30 @@ include 'Connection/common.php';
             $persentage=$Resultans*(100/$checked_count);
             $id=$_SESSION['id'];
             if($persentage>70){
-            $q="INSERT INTO user_exam_result (user_id ,c_1st) values ('$id','$persentage')";
+                
+            $query4 = "SELECT user_id   FROM  user_exam_result WHERE user_id = '" . $id . "'";
+            $result41 = mysqli_query($con, $query4) or die($mysqli_error($con));
+            $num = mysqli_num_rows($result41);
+            if($num==0){    
+                
+            $q="INSERT INTO user_exam_result (user_id ,$type) values ('$id','$persentage')";
             $q_submit= mysqli_query($con,$q) or die(mysqli_error($con));
              echo '<script>alert("Congrats, New Modules Unloacked")</script>';
-            // echo '<script>window.location="c_video_file.php"</script>';
+             echo '<script>window.location="c_video_file.php"</script>';
+            }
+            else{
+             
+                 $update_exam = "UPDATE user_exam_result SET $type='$persentage' WHERE user_id = '" . $id . "' ";
+                 $update_exam_result = mysqli_query($con, $update_exam) or die($mysqli_error($con));
+                 echo '<script>alert("Congrats, New Modules Unloacked")</script>';
+                 echo '<script>window.location="c_video_file.php"</script>';
+                
+                
+            }
             }
             else{
              echo '<script>alert("Better Luck Next Time, Try Again")</script>';
-             //echo '<script>window.location="c_video_file.php"</script>';
+            echo '<script>window.location="c_video_file.php"</script>';
             }
             
 ?>
