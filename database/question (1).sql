e4032a2f1fe9d60793550c32f6e9b53af6d97601
Sanchari99 <sanchari1999@gmail.com>
2021-07-20 10:40:39 +0530

    done

--- database/question (1).sql
+++ database/question (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jul 18, 2021 at 10:15 AM
+-- Generation Time: Jul 20, 2021 at 05:08 AM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS `question` (
   `type` varchar(255) NOT NULL,
   `course` varchar(255) NOT NULL,
   PRIMARY KEY (`qid`)
-) ENGINE=MyISAM AUTO_INCREMENT=31 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=46 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `question`
@@ -72,7 +72,22 @@ INSERT INTO `question` (`qid`, `question`, `ans_id`, `type`, `course`) VALUES
 (27, 'Which function of pre defined class Thread is used to check weather current thread being checked is still running?', 105, 'java_f', 'java'),
 (28, 'What is the process of defining more than one method in a class differentiated by parameters?', 110, 'java_f', 'java'),
 (29, 'Which of these is correct about passing an argument by call-by-value process?', 113, 'java_f', 'java'),
-(30, 'Which of these method is used to tell the calling thread to give up a monitor and go to sleep until some other thread enters the same monitor?', 117, 'java_f', 'java');
+(30, 'Which of these method is used to tell the calling thread to give up a monitor and go to sleep until some other thread enters the same monitor?', 117, 'java_f', 'java'),
+(31, 'Which of the following is called address operator?', 123, 'c_plus_1st', 'c_plus'),
+(32, 'What is the correct definition of an array?', 125, 'c_plus_1st', 'c_plus'),
+(33, 'Which of the following is illegal?', 131, 'c_plus_1st', 'c_plus'),
+(34, 'What are the parts of the literal constants?', 136, 'c_plus_1st', 'c_plus'),
+(35, 'How run-time polymorphisms are implemented in C++?', 140, 'c_plus_2nd', 'c_plus'),
+(36, 'What is a friend function in C++?', 141, 'c_plus_2nd', 'c_plus'),
+(37, 'What happens if a user forgets to define a constructor inside a class?', 148, 'c_plus_2nd', 'c_plus'),
+(38, 'What is the order of Constructors call when the object of derived class B is declared, provided class B is derived from class A?', 148, 'c_plus_2nd', 'c_plus'),
+(39, 'Which type of memory is used by an Array in C++ programming language?', 152, 'c_plus_f', 'c_plus'),
+(40, 'Which one of the following is the correct definition of the \"is_array();\" function in C++?', 156, 'c_plus_f', 'c_plus'),
+(41, 'Which of the following statement is correct about Virtual Inheritance?', 163, 'c_plus_f', 'c_plus'),
+(42, 'Which of the following statements is correct about the class?', 164, 'c_plus_f', 'c_plus'),
+(43, 'Which of the following is not a kind of inheritance?', 168, 'c_plus_f', 'c_plus'),
+(44, 'Which of the following refers to the wrapping of data and its functionality into a single individual entity?', 174, 'c_plus_f', 'c_plus'),
+(45, 'A function declared as the \" friend \" function can always access the data in _______.', 178, 'c_plus_f', 'c_plus');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
